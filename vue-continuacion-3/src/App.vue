a0e5c14e82c28e0df5351842173b44f1aa410497
Davy Jem <dscastanedav@unal.edu.co>
2022-07-17 01:32:40 -0500

    Montonazo de cosas, puro manejo de eventos y entradas del usuario a tra ves de un formulario con varios tipos de entrada y formas de conectar la informacion  del data script (pripiedades) con el html y volver al javasctip apuntando hacia los metodos con v-mode y muchas cosas mas

--- vue-continuacion-3/src/App.vue
+++ vue-continuacion-3/src/App.vue
@@ -1,32 +1,89 @@
 <template>
-  <h2>{{name}}</h2>
- <div>
-  <button v-on:mouseover="name = 'Dragonite'"> Cambiar nombre</button>
- </div>
-
- <h2>{{counter}}</h2>
- <div>
-  <button v-on:mouseover="counter += 1">Agregar 1 a la cuenta</button><hr />
-  <button v-on:mouseover="counter -= 1">Disminuir 1 a la cuenta</button>
- </div>
-<hr />
- <div>
-  <button @click="incrementar1"> Agrega 1 usando aumentar1</button>
-  <button @click="disminuir1"> Disminuye 1 usando disminuir1</button>
-  <button @click="incrementarNum(5)"> Agrega usando incrmentarNum</button>
-  <button @click="disminuirNum(5)"> Agrega usando desminuirNum</button>
- 
- </div>
-  <!--Every single botton goes under a div tag-->
- <div>
-  <button @mouseenter="cambiarNombre('Don Cachichurris', $event)" 
-  @mouseleave="cambiarNombre('Cabronidas Mendez', $event)">Cambiar el nombre</button>
-
-
+  <div>
+    <pre>
+      {{ JSON.stringify(formValues, null, 2) }}
+    </pre>
   </div>
 
+  <!--<form @submit="submitForm">-->
+  <form @submit.prevent="submitForm2">
+    <div>
+      <label for="name">Name</label>
+      <input type="text" id="name" v-model.trim.lazy="formValues.name">
+    </div>
+    
+    <div>
+      <label for="profile">Profile Summary</label>
+      <textarea id="profile" v-model="formValues.profileSummary" />  
+    </div>
+
+    <div>
+      <label for="country">Country</label>
+      <select id="country" v-model="formValues.country">
+        <option value="">Select an option</option>
+        <option value="Locombia">Locombia</option>
+        <option value="Polombia">Polombia</option>
+        <option value="Colombia">Colombia</option>
+        <option value="colombiaconp">Esa Colombia con P mayuscula</option>
+      </select>
+    </div>
+
+    <div>
+      <label for="platillo">Platillo</label>
+      <select id="platillo" multiple v-model="formValues.platillo">
+        <option value="cafe">Cafe</option>
+        <option value="pastalofra">Pastalofra</option>
+        <option value="sopa">Sopa</option>
+        <option value="torta">Torta</option>
+      </select>
+    </div>
+
+    <div>
+      <input type="checkbox" id="remote" v-model="formValues.remote">
+      <label for="remote"> Are you willing to work remotely?</label>
+    </div>
+
+    <div>
+      <input type="checkbox" id="estupidity" v-model="formValues.estupidez" true-value="si" false-value="no">
+      <label for="estupidity"> Es usted estupido?</label>
+    </div>
 
 
+    <div>
+      <label>Skill set</label>
+      <input type="checkbox" id="html" value="html" v-model="formValues.setSkills">
+      <label for="html">Html 5</label>
+      <input type="checkbox" id="css" value="css" v-model="formValues.setSkills">
+      <label for="css">CSS</label>
+      <input type="checkbox" id="javascript" value="javascript" v-model="formValues.setSkills">
+      <label for="javascript">Javascript</label>
+    </div>
+
+    <div>
+      <label>Years of war</label>
+      <input type="radio" id="0-2" value="0-2" v-model="formValues.yearsOfExperience">
+      <label for="0-2">0-2</label>
+      <input type="radio" id="3-5" value="3-5" v-model="formValues.yearsOfExperience">
+      <label for="3-5">3-5</label>
+      <input type="radio" id=">6" value=">6" v-model="formValues.yearsOfExperience">
+      <label for=">6">>6</label>
+    </div>
+    
+    <div>
+      <label for="age">Age</label>
+      <input @keyup.enter="submitForm2" type="number" id="age" v-model.number="formValues.age">
+    </div>
+
+    <!--<div>
+      <button>Submit crack!</button>
+    </div>-->
+
+    <!--   OJO   !!!!!!!-->
+    <!-- Hay que investigar sobre todos los modifiers, mouse modifiers,
+    keyboard modifiers, @keyup es el modificador para teclado creo
+    hay que investigar cual es el modificador para raton-->
+
+  </form>
 
 </template>
 
@@ -35,28 +92,27 @@ export default {
   name: 'App',
   data(){
     return {
-      name: 'Davy Jem',
-      counter: 0
+      formValues:{
+        name: '',
+        profileSummary: '',
+        country: '',
+        platillo: '',
+        remoteWork: false,
+        estupidez: 'yes',
+        setSkills: [],
+        yearsOfExperience: '',
+        age: null
+      }
     }
   },
   methods: {
-    cambiarNombre(nam, event){
-      this.name = nam
-      console.log('Event', event)
+    submitForm(event){
+      event.preventDefault();
+      console.log('Form values', this.formValues)
     },
-   incrementar1(){
-    this.counter += 1
-   },
-   incrementarNum(num){
-    this.counter += num
-   },
-   disminuir1(){
-    this.counter -= 1 
-   },
-   disminuirNum(num){
-    this.counter -= num
-   }
-
+    submitForm2(){
+      console.log('Form values', this.formValues)
+    }
 
   },
   
@@ -69,7 +125,44 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
   margin-top: 60px;
+}
+.underline {
+  text-decoration: underline;
+}
+.promoted {
+  font-style: italic;
+}
+.new {
+  color: olivedrab;
+}
+.sold-out {
+  color: red;
+}
+label {
+  font-weight: bold;
+  display: flex;
+  margin-bottom: 5px;
+}
+input + label {
+  font-weight: bold;
+  display: inline-flex;
+  margin-right: 20px;
+}
+input[type='text'],
+textarea,
+select {
+  display: block;
+  width: 400px;
+  padding: 6px 12px;
+  font-size: 14px;
+  line-height: 1.42857143;
+  color: #555;
+  background-color: #fff;
+  background-image: none;
+  border: 1px solid #ccc;
+  border-radius: 4px;
 }
 </style>
